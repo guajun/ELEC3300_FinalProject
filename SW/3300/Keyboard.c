ca47b1a7c0ab7b5e00a5ba383563b0660a5621f5
gua_jun <whgls323232@gmail.com>
2023-04-18 16:41:46 +0800

    successfully read keyboard data

---
+++ SW/3300/Keyboard.c
@@ -0,0 +1,16 @@
+#include "Keyboard,h"
+
+#include "usbh_hid.h"
+#include "usbh_hid_keybd.h"
+#include "usb_host.h"
+
+void USBH_HID_EventCallback(USBH_HandleTypeDef *phost)
+{
+    if(USBH_HID_GetDeviceType(phost) == HID_KEYBOARD)  // if the HID is Mouse
+	{
+		static char key;
+		static HID_KEYBD_Info_TypeDef *keyboardInfo;
+		keyboardInfo = USBH_HID_GetKeybdInfo(phost);  // get the info
+		key = USBH_HID_GetASCIICode(keyboardInfo);  // get the key pressed
+	}
+}
