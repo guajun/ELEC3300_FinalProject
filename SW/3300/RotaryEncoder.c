d96f5146bc76b4de5f59f9a4e09e475235f5bc79
赵钰丹 <yudanzhao92@gmail.com>
2023-04-18 19:48:19 +0800

    create feature_RotaryEncoder

---
+++ SW/3300/RotaryEncoder.c
@@ -0,0 +1,13 @@
+#include "RotaryEncoder.h"
+#include "tim.h"
+
+#include <stdint.h>
+void RotaryEncoder_init()
+{
+    HAL_TIM_Encoder_Start(&htim1, TIM_CHANNEL_1 | TIM_CHANNEL_2);
+}
+
+uint32_t RotaryEncoder_read()
+{
+    return __HAL_TIM_GET_COUNTER(&htim1);
+}
