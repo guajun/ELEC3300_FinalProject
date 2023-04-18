0fc843b2b532199ec53eb02bbeae55ab21a1d29f
gua_jun <whgls323232@gmail.com>
2023-04-18 17:00:42 +0800

    init test

---
+++ SW/3300/SDCard.c
@@ -0,0 +1,22 @@
+#include "SDCard.h"
+
+#include "ff.h"
+#include "fatfs.h"
+
+FRESULT res; /* FatFs function common result code */
+uint32_t byteswritten;
+uint8_t wtext[] = "STM32 FATFS works great!";
+
+
+void SDCard_init()
+{
+    res = f_mount(&SDFatFS, (TCHAR const*)SDPath, 0);
+
+    res = f_open(&SDFile, "STM32.TXT", FA_CREATE_ALWAYS | FA_WRITE);
+
+    res = f_write(&SDFile, wtext, strlen((char *)wtext), (void *)&byteswritten);
+
+    res = f_close(&SDFile);
+
+    res = f_mount(&SDFatFS, (TCHAR const*)NULL, 0);
+}
