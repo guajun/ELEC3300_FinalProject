1fea9adf49b070051eec39e318645415fe8a8e54
赵钰丹 <yudanzhao92@gmail.com>
2023-04-18 20:21:04 +0800

    modified RotaryEncoder.c

--- SW/3300/RotaryEncoder.c
+++ SW/3300/RotaryEncoder.c
@@ -4,10 +4,10 @@
 #include <stdint.h>
 void RotaryEncoder_init()
 {
-    HAL_TIM_Encoder_Start(&htim1, TIM_CHANNEL_1 | TIM_CHANNEL_2);
+    HAL_TIM_Encoder_Start(&htim2, TIM_CHANNEL_1 | TIM_CHANNEL_2);
 }
 
 uint32_t RotaryEncoder_read()
 {
-    return __HAL_TIM_GET_COUNTER(&htim1);
+    return __HAL_TIM_GET_COUNTER(&htim2);
 }
