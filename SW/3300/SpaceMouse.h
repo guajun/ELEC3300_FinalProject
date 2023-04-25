189a246dac1d3609fe9623138efe5045120fb5f1
gua_jun <whgls323232@gmail.com>
2023-04-25 12:34:55 +0800

    pass compile

--- SW/3300/SpaceMouse.h
+++ SW/3300/SpaceMouse.h
@@ -1,9 +1,9 @@
 #pragma once
 
-void ZeroPosition_Set();
-void AutoData_On();
-void AutoData_Off();
-void RequestData_Send(uint8_t* RecByteArray);
-uint16_t AxisValue_get(char a);
+// void ZeroPosition_Set();
+// void AutoData_On();
+// void AutoData_Off();
+// void RequestData_Send(uint8_t* RecByteArray);
+// uint16_t AxisValue_get(char a);
 
 void SpaceMouse_init();
