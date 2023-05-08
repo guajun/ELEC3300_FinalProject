0df830b8f3871c378b0f723550a52f92b04771b2
gua_jun <whgls323232@gmail.com>
2023-05-08 11:49:17 +0800

    finish encoder adjust speed

--- SW/3300/OS.c
+++ SW/3300/OS.c
@@ -13,66 +13,83 @@
 #include "Keyboard.h"
 
 
-#define DM4310_POS_STEP 0.001
-#define HT4310_POS_STEP 20
+#define DM4310_POS_STEP 0.0004
+#define HT4310_POS_STEP 10
 #define GO_POS_STEP 0.0001
 
 float OS_tickFreq = 0;
 
 static void keyboardMotor()
 {
+    static float k;
+    uint32_t encoder = RotaryEncoder_read();
+
+    uint32_t num = encoder >> 2;
+
+    if((int)encoder < 0)
+    {
+        num = 0;
+    }
+    else if(num > 15)
+    {
+        num = 15;
+    }
+
+    k = 1 + num * 0.25;
+    // k = (float)(((int)RotaryEncoder_read()) > 0 ? RotaryEncoder_read() : 0) / 
+
     if(keys.Q == KEY_STATE_PRESSED)
     {
-        DM4310_insts[0].control.position += DM4310_POS_STEP;
+        DM4310_insts[0].control.position += DM4310_POS_STEP * k;
     }
     else if(keys.A == KEY_STATE_PRESSED)
     {
-        DM4310_insts[0].control.position -= DM4310_POS_STEP;
+        DM4310_insts[0].control.position -= DM4310_POS_STEP * k;
     }
 
     if(keys.W == KEY_STATE_PRESSED)
     {
-        DM4310_insts[1].control.position += DM4310_POS_STEP;
+        DM4310_insts[1].control.position += DM4310_POS_STEP * k;
     }
     else if(keys.S == KEY_STATE_PRESSED)
     {
-        DM4310_insts[1].control.position -= DM4310_POS_STEP;
+        DM4310_insts[1].control.position -= DM4310_POS_STEP * k;
     }
 
     if(keys.E == KEY_STATE_PRESSED)
     {
-        HT4310_insts[0].control.position += HT4310_POS_STEP;
+        HT4310_insts[0].control.position += HT4310_POS_STEP * k;
     }
     else if(keys.D == KEY_STATE_PRESSED)
     {
-        HT4310_insts[0].control.position -= HT4310_POS_STEP;
+        HT4310_insts[0].control.position -= HT4310_POS_STEP * k;
     }
 
     if(keys.R == KEY_STATE_PRESSED)
     {
-        HT4310_insts[1].control.position += HT4310_POS_STEP;
+        HT4310_insts[1].control.position += HT4310_POS_STEP * k;
     }
     else if(keys.F == KEY_STATE_PRESSED)
     {
-        HT4310_insts[1].control.position -= HT4310_POS_STEP;
+        HT4310_insts[1].control.position -= HT4310_POS_STEP * k;
     }
 
     if(keys.T == KEY_STATE_PRESSED)
     {
-        GO_M8010_6_insts[0].tarPos += GO_POS_STEP;
+        GO_M8010_6_insts[0].tarPos += GO_POS_STEP * k;
     }
     else if(keys.G == KEY_STATE_PRESSED)
     {
-        GO_M8010_6_insts[0].tarPos -= GO_POS_STEP;
+        GO_M8010_6_insts[0].tarPos -= GO_POS_STEP * k;
     }
 
     if(keys.Y == KEY_STATE_PRESSED)
     {
-        GO_M8010_6_insts[1].tarPos += GO_POS_STEP;
+        GO_M8010_6_insts[1].tarPos += GO_POS_STEP * k;
     }
     else if(keys.H == KEY_STATE_PRESSED)
     {
-        GO_M8010_6_insts[1].tarPos -= GO_POS_STEP;
+        GO_M8010_6_insts[1].tarPos -= GO_POS_STEP * k;
     }
 }
 
@@ -97,7 +114,7 @@ static void thread(TIM_HandleTypeDef *htim)
     OS_tickFreq = (HAL_GetTick() - lastTick) / 0.001;
     lastTick = HAL_GetTick();
     
-    static enum Key_State lastLKeyState = KEY_STATE_RELEASED;
+    // static enum Key_State lastLKeyState = KEY_STATE_RELEASED;
 
     // if(lastLKeyState != keys.L && key)
 
